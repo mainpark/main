808864f60cbc4aea0830c85dc9e5c0095812f545
mainpark <100174397+mainpark@users.noreply.github.com>
2025-11-17 22:58:44 +0900

    Update hbc.js

--- netlify/functions/hbc.js
+++ netlify/functions/hbc.js
@@ -1,37 +1,37 @@
-export default async () => {
-  try {
-    const url = "https://www.lbank.com/v2/kline.do?symbol=hb_usdt&type=day&size=200";
+export default async (req) => {
+  const url = "https://www.lbank.com/v2/kline.do?symbol=hb_usdt&type=day1&size=200";
 
-    const response = await fetch(url, {
+  try {
+    const res = await fetch(url, {
       headers: { "accept": "application/json" }
     });
 
-    const json = await response.json();
+    const json = await res.json();
 
-    if (!json || !json.data) {
-      return new Response(JSON.stringify({
-        error: true,
-        message: "No candle data returned"
-      }), { status: 500 });
+    if (!json?.data) {
+      return new Response(JSON.stringify({ error: true, message: "NO DATA" }), {
+        status: 500,
+        headers: { "Content-Type": "application/json" }
+      });
     }
 
     const candles = json.data.map(c => ({
       time: c[0] / 1000,
-      open: parseFloat(c[1]),
-      close: parseFloat(c[2]),
-      high: parseFloat(c[3]),
-      low: parseFloat(c[4]),
+      open: Number(c[1]),
+      high: Number(c[3]),
+      low: Number(c[4]),
+      close: Number(c[2])
     }));
 
-    return new Response(JSON.stringify(candles), {
+    return new Response(JSON.stringify({ candles }), {
       headers: { "Content-Type": "application/json" }
     });
 
   } catch (e) {
-    return new Response(JSON.stringify({
-      error: true,
-      message: e.message
-    }), { status: 500 });
+    return new Response(JSON.stringify({ error: true, message: e.message }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" }
+    });
   }
 };
 
