9b11ee1a85ea1b2f33a61bd1611e20d33ab92568
mainpark <100174397+mainpark@users.noreply.github.com>
2025-11-17 19:01:25 +0900

    Update hbc.js

--- netlify/functions/hbc.js
+++ netlify/functions/hbc.js
@@ -18,3 +18,24 @@ export default async (request, context) => {
     });
   }
 };
+import fetch from "node-fetch";
+
+export const handler = async () => {
+  try {
+    const url = "https://www.lbank.com/v2/hist?symbol=hb_usdt&type=1day";
+
+    const res = await fetch(url);
+    const json = await res.json();
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(json),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: true, message: e.message }),
+    };
+  }
+};
